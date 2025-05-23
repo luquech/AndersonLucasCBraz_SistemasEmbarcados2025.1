2b545743d133112d4ed89596833fc0cb3d7e32b1
Anderson Lucas Cordeiro Braz <79807018+luquech@users.noreply.github.com>
2025-05-23 08:20:37 -0300

    Update main.c

--- atividades/Atividade 5/main.c
+++ atividades/Atividade 5/main.c
@@ -15,7 +15,7 @@ uint8_t contador = 0;
 uint8_t soma = 1;
 uint64_t ultimo_tempo_botao1 = 0;
 uint64_t ultimo_tempo_botao2 = 0;
-uint64_t debounce_time = 100000;
+const uint64_t debounce_time = 100000;
 
 void atualizar_leds() {
     gpio_set_level(LED0, contador & 1);
@@ -45,21 +45,29 @@ void configurar_gpio() {
     gpio_set_intr_type(BOTAO2, GPIO_INTR_NEGEDGE);
 }
 
-static void IRAM_ATTR gpio_isr_handler(void* arg) {
-    uint32_t gpio_num = (uint32_t) arg;
+static void IRAM_ATTR botao1_handler(void* arg) {
     uint64_t agora = esp_timer_get_time();
-    
-    if (gpio_num == BOTAO1) {
-        if ((agora - ultimo_tempo_botao1) > debounce_time) {
-            ultimo_tempo_botao1 = agora;
-            contador = (contador < 15) ? contador + soma : soma - 1;
-            if (contador > 15) contador -= 16;
+    if ((agora - ultimo_tempo_botao1) > debounce_time) {
+        ultimo_tempo_botao1 = agora;
+        if (contador < 15) {
+            contador = contador + soma;
+            if (contador > 15) {
+                contador = contador - 16;
+            }
+        } else {
+            contador = soma - 1;
         }
-    } 
-    else if (gpio_num == BOTAO2) {
-        if ((agora - ultimo_tempo_botao2) > debounce_time) {
-            ultimo_tempo_botao2 = agora;
-            soma = (soma == 1) ? 2 : 1;
+    }
+}
+
+static void IRAM_ATTR botao2_handler(void* arg) {
+    uint64_t agora = esp_timer_get_time();
+    if ((agora - ultimo_tempo_botao2) > debounce_time) {
+        ultimo_tempo_botao2 = agora;
+        if (soma == 1) {
+            soma = 2;
+        } else {
+            soma = 1;
         }
     }
 }
@@ -68,11 +76,12 @@ void app_main() {
     configurar_gpio();
     atualizar_leds();
     gpio_install_isr_service(0);
-    gpio_isr_handler_add(BOTAO1, gpio_isr_handler, (void*) BOTAO1);
-    gpio_isr_handler_add(BOTAO2, gpio_isr_handler, (void*) BOTAO2);
+    gpio_isr_handler_add(BOTAO1, botao1_handler, NULL);
+    gpio_isr_handler_add(BOTAO2, botao2_handler, NULL);
 
     while(1) {
         atualizar_leds();
-        vTaskDelay(pdMS_TO_TICKS(10));
+        printf("Contador: %d, Soma: %d\n", contador, soma);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
